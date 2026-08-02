890c233706a959768ebfefe26bde02edcf2549a8
Lennert <lennertgavel@gmail.com>
2026-08-02 18:04:36 +0200

    fix mobile version

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -2,9 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Uncomment and fill in for GitHub Pages deployment:
-  // site: 'https://lennertgavel.github.io',
-  // base: '/lennertgavel.github.io/',
   output: 'static',
   publicDir: 'assets',
 });
